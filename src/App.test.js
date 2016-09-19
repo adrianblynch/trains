c58b01ba4feac13291ebd9ac57477e761c101e26
Adrian Lynch <adi_ady_ade@hotmail.com>
2016-09-19 08:08:36 +0100

    Add App unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const html = ReactDOM.render(<App />, div);
+  console.log(html)
 });
